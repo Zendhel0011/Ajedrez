46313c28b7f0b757c220c398eed501abd6b4fa33
Ezi <exio78@hotmail.com>
2021-08-26 21:10:38 -0300

    Cambiado metodo de almacenar piezas, añadido atributo img a las piezas

--- js/init.js
+++ js/init.js
@@ -1,55 +1,148 @@
+//Saque esto para utilizarlo tambien en otras funciones
+
+const letter = {
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H"
+};
+
+const positions = {};
+
 document.addEventListener("DOMContentLoaded", () => {
-    const game = document.getElementById("game")
+    const game = document.getElementById("game");
     let color = true;
-    let casillas = `<div class="casilla" id=""></div>`
-    let letter = {
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
-        5: "E",
-        6: "F",
-        7: "G",
-        8: "H"
-    }
+    let casillas = `<div class="casilla" id=""></div>`;
     for (let i = 8; i >= 1; i--) {
         for (let j = 1; j <= 8; j++) {
             if (!color) {
-                casillas = `<div class="casilla casilla--black" id="${letter[j]}${i}"></div>`
+                casillas = `<div class="casilla casilla--black" id="${letter[j]}${i}"></div>`;
             } else {
-                casillas = `<div class="casilla casilla--white" id="${letter[j]}${i}"></div>`
+                casillas = `<div class="casilla casilla--white" id="${letter[j]}${i}"></div>`;
             }
-            game.innerHTML += casillas
-            color = !color
+            game.innerHTML += casillas;
+            color = !color;
+            positions[`${letter[j]}${i}`] = "";
         }
-        color = !color
+        color = !color;
     }
-})
+});
 
+//Crea las piezas y las ordena en categorias
 
-function createPiece() {
-    let white_pieces = [];
-    let black_pieces = [];
+const createPieces = () => {
+    let white_pieces = {};
+    let black_pieces = {};
+
+    white_pieces.pawns = [];
+    black_pieces.pawns = [];
+
     for (let i = 0; i <= 7; i++) {
-        white_pieces.push(new Peon(`Wpawn${i}`, 1));
-        black_pieces.push(new Peon(`Bpawn${i}`, 0));
+        white_pieces.pawns.push(new Peon(`Wpawn${i}`, 1));
+        black_pieces.pawns.push(new Peon(`Bpawn${i}`, 0));
     }
+
+    white_pieces.knights = [];
+    white_pieces.rooks = [];
+    white_pieces.bishops = [];
+    black_pieces.knights = [];
+    black_pieces.rooks = [];
+    black_pieces.bishops = [];
+
     for (let i = 0; i <= 1; i++) {
-        white_pieces.push(new Caballo(`Wknight${i}`, 1));
-        white_pieces.push(new Torre(`Wrook${i}`, 1));
-        white_pieces.push(new Alfil(`Wbishop${i}`, 1));
-        black_pieces.push(new Caballo(`Bknight${i}`, 0));
-        black_pieces.push(new Torre(`Brook${i}`, 1));
-        black_pieces.push(new Alfil(`Bbishop${i}`, 1));
+        white_pieces.knights.push(new Caballo(`Wknight${i}`, 1));
+        white_pieces.rooks.push(new Torre(`Wrook${i}`, 1));
+        white_pieces.bishops.push(new Alfil(`Wbishop${i}`, 1));
+        black_pieces.knights.push(new Caballo(`Bknight${i}`, 0));
+        black_pieces.rooks.push(new Torre(`Brook${i}`, 0));
+        black_pieces.bishops.push(new Alfil(`Bbishop${i}`, 0));
     }
-    white_pieces.push(new Rey(`Wking`, 1))
-    white_pieces.push(new Reina(`Wqueen`, 1))
-    black_pieces.push(new Rey(`Bking`, 0))
-    black_pieces.push(new Reina(`Bqueen`, 0))
 
-    return [white_pieces, black_pieces];
+    white_pieces.king = new Rey(`Wking`, 1);
+    white_pieces.queen = new Reina(`Wqueen`, 1);
+    black_pieces.king = new Rey(`Bking`, 0);
+    black_pieces.queen = new Reina(`Bqueen`, 0);
+
+    return {white : white_pieces, black : black_pieces};
 }
 
-function startGame() {
+let pieces = createPieces();
 
+const startGame = () => {
+
+    //Se posicionan las piezas blancas
+
+    pieces.white.rooks[0].position = "A1";
+    positions.A1 = pieces.white.rooks[0].id;
+
+    pieces.white.knights[0].position = "B1";
+    positions.B1 = pieces.white.knights[0].id;
+
+    pieces.white.bishops[0].position = "C1";
+    positions.C1 = pieces.white.bishops[0].id;
+
+    pieces.white.queen.position = "D1";
+    positions.D1 = pieces.white.queen.id;
+
+    pieces.white.king.position = "E1";
+    positions.E1 = pieces.white.king.id;
+
+    pieces.white.bishops[1].position = "F1";
+    positions.F1 = pieces.white.bishops[1].id;
+
+    pieces.white.knights[1].position = "G1";
+    positions.G1 = pieces.white.knights[1].id;
+
+    pieces.white.rooks[1].position = "H1";
+    positions.H1 = pieces.white.rooks[1].id;
+
+    //Se posicionan las piezas negras
+
+    pieces.black.rooks[0].position = "A8";
+    positions.A8 = pieces.black.rooks[0].id;
+
+    pieces.black.knights[0].position = "B8";
+    positions.B8 = pieces.black.knights[0].id;
+
+    pieces.black.bishops[0].position = "C8";
+    positions.C8 = pieces.black.bishops[0].id;
+
+    pieces.black.queen.position = "D8";
+    positions.D8 = pieces.black.queen.id;
+
+    pieces.black.king.position = "E8";
+    positions.E8 = pieces.black.king.id;
+
+    pieces.black.bishops[1].position = "F8";
+    positions.F8 = pieces.black.bishops[1].id;
+
+    pieces.black.knights[1].position = "G8";
+    positions.G8 = pieces.black.knights[1].id;
+
+    pieces.black.rooks[1].position = "H8";
+    positions.H8 = pieces.black.rooks[1].id;
+
+    //Se posicionan los peones
+
+    for (let i = 1; i <= 8; i++) {
+        pieces.white.pawns[i-1].position = `${letter[i]}2`;
+        positions[`${letter[i]}2`] = pieces.white.pawns[i-1].id;
+
+        pieces.white.pawns[i-1].position = `${letter[i]}7`;
+        positions[`${letter[i]}7`] = pieces.black.pawns[i-1].id;
+    }
+
+    for (let i = 1; i <= 8; i++) {
+
+    }
 }
+
+/*const print = (position) => {
+    document.getElementById(position).innerHTML = `
+    <img src=${}>
+    `
+}*/
--- js/piezas.js
+++ js/piezas.js
@@ -1,51 +1,52 @@
 class Pieza {
-    constructor(id, color, position = null, state = "Alive") {
+    constructor(id, color, position = null, state = "Alive", img = "") {
         this.id = id;
         this.color = color;
         this.position = position;
         this.state = state;
+        this.img = img;
     }
 
 }
 
 class Peon extends Pieza {
-    constructor(id, color, position = null, state = "Alive") {
-        super(id, color, position = null, state = "Alive");
+    constructor(id, color, position = null, state = "Alive", img = `img/pawn_${color}`) {
+        super(id, color, position, img, state, img);
     }
 
 }
 
 class Torre extends Pieza {
-    constructor(id, color, position = null, state = "Alive") {
-        super(id, color, position = null, state = "Alive");
+    constructor(id, color, position = null, state = "Alive", img = `img/rook_${color}`) {
+        super(id, color, position, state, img);
     }
 
 }
 
 class Caballo extends Pieza {
-    constructor(id, color, position = null, state = "Alive") {
-        super(id, color, position = null, state = "Alive");
+    constructor(id, color, position = null, state = "Alive", img = `img/knight_${color}`) {
+        super(id, color, position, state, img);
     }
 
 }
 
 class Reina extends Pieza {
-    constructor(id, color, position = null, state = "Alive") {
-        super(id, color, position = null, state = "Alive");
+    constructor(id, color, position = null, state = "Alive", img = `img/queen_${color}`) {
+        super(id, color, position, state, img);
     }
 
 }
 
 class Rey extends Pieza {
-    constructor(id, color, position = null, state = "Alive") {
-        super(id, color, position = null, state = "Alive");
+    constructor(id, color, position = null, state = "Alive", img = `img/king_${color}`) {
+        super(id, color, position, state, img);
     }
 
 }
 
 class Alfil extends Pieza {
-    constructor(id, color, position = null, state = "Alive") {
-        super(id, color, position = null, state = "Alive");
+    constructor(id, color, position = null, state = "Alive", img = `img/bishop_${color}`) {
+        super(id, color, position = null, state, img);
     }
 
 }
